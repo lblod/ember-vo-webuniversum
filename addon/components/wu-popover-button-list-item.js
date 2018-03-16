f6997b966e2b2ee0cdd0b89672d59952c6c711ce
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-16 14:14:40 +0100

    Add popover component using Ember contextual components

---
+++ addon/components/wu-popover-button-list-item.js
@@ -0,0 +1,16 @@
+import Component from '@ember/component';
+import { assert } from '@ember/debug';
+import layout from '../templates/components/wu-popover-button-list-item';
+
+export default Component.extend({
+  layout,
+  tagName: 'li',
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    assert('{{wu-popover-button-list-item}} requires an `onClick` action or null for no action.', this.get('onClick') !== undefined);
+  },
+  click() {
+    this.sendAction('onClick'); // eslint-disable-line ember/closure-actions
+  }
+});
