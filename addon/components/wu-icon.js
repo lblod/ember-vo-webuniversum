cbadca1eab7938315d8a8a3e075f1073d6dd696d
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-27 15:21:55 +0200

    Add support for font-awesome icons

--- addon/components/wu-icon.js
+++ addon/components/wu-icon.js
@@ -1,13 +1,24 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import layout from '../templates/components/wu-icon';
 
 let WuIconComponent = Component.extend({
   layout,
   tagName: 'i',
-  classNames: ['vi'],
-  classNameBindings: ['iconClass'],
-  iconClass: alias('icon')
+  classNames: [],
+  classNameBindings: [
+    'iconClass',
+    'isFaIcon:fa',
+    'isViIcon:vi'
+  ],
+  iconClass: alias('icon'),
+  isFaIcon: computed('icon', function() {
+    return this.get('icon') && this.get('icon').startsWith('fa-');
+  }),
+  isViIcon: computed('icon', function() {
+    return this.get('icon') && this.get('icon').startsWith('vi-');
+  })
 });
 
 WuIconComponent.reopenClass({
