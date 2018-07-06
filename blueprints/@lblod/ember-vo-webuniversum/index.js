b5925fcf4010649bf150acf88c02d3a76042f4ec
Niels Vandekeybus <progster@gmail.com>
2018-07-06 11:00:38 +0200

    added styled error, loading and not-found routes
    this way vo templates only have to be modified in one place

--- blueprints/@lblod/ember-vo-webuniversum/index.js
+++ blueprints/@lblod/ember-vo-webuniversum/index.js
@@ -1,9 +1,14 @@
 /* eslint-env node */
+var EmberRouterGenerator = require('ember-router-generator');
+
 module.exports = {
   description: '',
   async afterInstall(options) {
-    //This will follow major versions until 4
+    const source = path.join.apply(options.project.root, 'app', 'router.js');
+    const routes = new EmberRouterGenerator(source);
+    routes.add('route-not-found', {path: '/*wildcard'});
     this.addPackageToProjec('postscribe', '^2.0.8');
+    //This will follow major versions until 4
     return this.addAddonToProject('ember-cli-moment-shim', '~3.7.1');
   }
 };
