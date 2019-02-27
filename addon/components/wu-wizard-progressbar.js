33c1090e1be80fb57c3ac7502aeeeb962a7d930b
Claire Lovisa <lovisa.claire@gmail.com>
2019-02-27 09:59:47 +0100

    Fix linting error

--- addon/components/wu-wizard-progressbar.js
+++ addon/components/wu-wizard-progressbar.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import layout from '../templates/components/wu-wizard-progressbar';
-import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
